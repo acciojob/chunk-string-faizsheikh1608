b9a03d0eedec71cea433cbcb60db29b91f040874
faizsheikh1608 <164844564+faizsheikh1608@users.noreply.github.com>
2024-12-28 11:50:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function stringChop(str, size) {
   // your code here
+	const arr = [];
+	let count = size;
+	let subStr = '';
+	for(int i = 0 ; i < str.length;i++){
+		if(count === 0){
+			arr.push(subStr);
+			subStr = '';
+			count = size;
+		}
+		subStr = str[i];
+		count--;
+	}
+
+	arr.push(subStr);
+	return arr;
 }
 
 // Do not change the code below
